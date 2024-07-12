f651af776f3e3d2ae43ce045eda4fbe0612a90bf
Murph <murph@opusdavei.org>
2024-07-12 17:22:01 +0100

    remove unused version
    Signed-off-by: Murph <murph@opusdavei.org>

--- internal/version/version.go
+++
@@ -1,5 +0,0 @@
-// Package version contains the version of provider-nop
-package version
-
-// Version will be overridden with the current version at build time using the -X linker flag
-var Version = "0.0.0"
